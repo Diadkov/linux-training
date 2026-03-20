444ace8d79725ec50e2d813714d36c55233cd8b3
Diadkov <v1k2rdiadkov@gmail.com>
2026-03-20 14:34:28 +0000

    Add malloc free functions using sbrk function

--- common/errorFunctions.h
+++ common/errorFunctions.h
@@ -1,6 +1,4 @@
 #define _GNU_SOURCE
-#ifndef ERROR_FUNCTIONS_H
-#define ERROR_FUNCTIONS_H
 
 #include <stdarg.h>
 
@@ -28,6 +26,17 @@
 #undef FALSE
 #endif
 
+#ifndef GET_NUM_H
+#define GET_NUM_H
+
+#define GN_NONNEG       01      /* Value must be >= 0 */
+#define GN_GT_0         02      /* Value must be > 0 */
+
+                                /* By default, integers are decimal */
+#define GN_ANY_BASE   0100      /* Can use any base - like strtol(3) */
+#define GN_BASE_8     0200      /* Value is expressed in octal */
+#define GN_BASE_16    0400      /* Value is expressed in hexadecimal */
+
 typedef enum { FALSE, TRUE } Boolean;
 
 /* Function prototypes */
@@ -47,3 +56,39 @@ void usageErr(const char *format, ...);
 void cmdLineErr(const char *format, ...);
 
 #endif
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+/* Print a diagnostic message that contains a function name ('fname'),
+   the value of a command-line argument ('arg'), the name of that
+   command-line argument ('name'), and a diagnostic error message ('msg'). */
+
+static void
+gnFail(const char *fname, const char *msg, const char *arg, const char *name);
+/* Convert a numeric command-line argument ('arg') into a long integer,
+   returned as the function result. 'flags' is a bit mask of flags controlling
+   how the conversion is done and what diagnostic checks are performed on the
+   numeric result; see get_num.h for details.
+
+   'fname' is the name of our caller, and 'name' is the name associated with
+   the command-line argument 'arg'. 'fname' and 'name' are used to print a
+   diagnostic message in case an error is detected when processing 'arg'. */
+
+static long
+getNum(const char *fname, const char *arg, int flags, const char *name);
+
+/* Convert a numeric command-line argument string to a long integer. See the
+   comments for getNum() for a description of the arguments to this function. */
+
+long
+getLong(const char *arg, int flags, const char *name);
+
+/* Convert a numeric command-line argument string to an integer. See the
+   comments for getNum() for a description of the arguments to this function. */
+
+int
+getInt(const char *arg, int flags, const char *name);
